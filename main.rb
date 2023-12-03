b719c72a9401027a85bb6c09dd06c1fdca55df78
kenziekhairy <29974502-kenziekhairy@users.noreply.replit.com>
2023-12-03 13:35:36 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,6 @@
+puts "Kamu suka makanan apa ?"
+makanan = gets 
+puts "saya suka makan #{makanan}"
+puts "Sudah berapa lama kamu makan makakan itu #{makanan} ?"
+berapa_lama = gets.to_i
+puts "Saya makan makanan itu #{makanan} selama #{berapa_lama} tahun"
